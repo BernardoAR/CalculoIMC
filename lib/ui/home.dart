6ffe6c880c4a787f779b0a8c3d96ad1911ae84da
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2020-01-22 02:01:38 -0300

    Criado lógica para o cálculo do IMC e apresentação na tela.
    Signed-off-by: BernardoAR <bernardo.roballo@aluno.iffar.edu.br>

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,4 @@
+import 'package:calculo_peso/ui/alerta.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,10 +10,82 @@ class _HomeState extends State<Home> {
   final TextEditingController _controleIdade = TextEditingController();
   final TextEditingController _controleAltura = TextEditingController();
   final TextEditingController _controlePeso = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final _form = GlobalKey<FormState>();
+  final Alerta _alerta = Alerta();
+
+  /// Método para cálculo do IMC
   void _calculaIMC() {
-    // Verifica se os campos correspondem
+    setState(() {
+      // Pega os dados e colocam em variáveis locais
+      double _alturaTmp = double.parse(_controleAltura.text);
+      double _pesoTmp = double.parse(_controlePeso.text);
+
+      // Faz o cálculo do IMC
+      double calculoImc = ((_pesoTmp) / (_alturaTmp * _alturaTmp));
+      _verificaIMC(calculoImc);
+    });
   }
+
+  /// Método para verificação do IMC
+  void _verificaIMC(double valor) {
+    String _tipo = '';
+    if (valor < 17) {
+      _tipo = 'Muito abaixo do Peso';
+    } else if (valor >= 17 && valor < 18.5) {
+      _tipo = 'Abaixo do Peso';
+    } else if (valor >= 18.5 && valor < 25) {
+      _tipo = 'Peso normal';
+    } else if (valor >= 25 && valor < 30) {
+      _tipo = 'Obesidade I';
+    } else if (valor >= 35 && valor < 40) {
+      _tipo = 'Obesidade II (severa)';
+    } else {
+      _tipo = 'Obesidade III (mórbida)';
+    }
+    // Manda pro alerta
+    _alerta.alerta(
+        context: context,
+        texto:
+            'O valor do seu IMC foi ${valor.toStringAsPrecision(2)}. \nE o normal é entre 18.50 e 24.99 \n ',
+        titulo: _tipo);
+  }
+
+  /// Método utilizado para validar a idade
+  String _validaIdade(valor) {
+    if (valor.isEmpty) {
+      return 'Campo idade não pode ser vazio';
+    }
+    if (valor is double) {
+      return 'Campo idade não pode ser decimal';
+    }
+    if (int.parse(valor) <= 0) {
+      return 'Por favor, entre com uma idade válida';
+    }
+    return null;
+  }
+
+  /// Método utilizado para validar a altura
+  String _validaAltura(valor) {
+    if (valor.isEmpty) {
+      return 'Campo altura não pode ser vazio';
+    }
+    if (double.parse(valor) <= 0) {
+      return 'Por favor, entre com uma altura válida';
+    }
+    return null;
+  }
+
+  /// Método utilizado para validar o peso
+  String _validaPeso(valor) {
+    if (valor.isEmpty) {
+      return 'Campo peso não pode ser vazio';
+    }
+    if (double.parse(valor) <= 0) {
+      return 'Por favor, entre com um peso válido';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +101,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Container(
                     child: Form(
-                  key: _formKey,
+                  key: _form,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
@@ -44,15 +117,7 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.all(4.0),
                         child: TextFormField(
                             controller: _controleIdade,
-                            validator: (valor) {
-                              if (valor.isEmpty) {
-                                return 'Campo idade não pode ser vazio';
-                              }
-                              if (int.parse(valor) <= 0) {
-                                return 'Por favor, entre com uma idade válida';
-                              }
-                              return null;
-                            },
+                            validator: _validaIdade,
                             keyboardType: TextInputType.numberWithOptions(
                                 signed: true, decimal: false),
                             decoration: InputDecoration(
@@ -65,15 +130,7 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.all(4.0),
                         child: TextFormField(
                             controller: _controleAltura,
-                            validator: (valor) {
-                              if (valor.isEmpty) {
-                                return 'Campo altura não pode ser vazio';
-                              }
-                              if (double.parse(valor) <= 0) {
-                                return 'Por favor, entre com uma altura válida';
-                              }
-                              return null;
-                            },
+                            validator: _validaAltura,
                             keyboardType: TextInputType.numberWithOptions(
                                 signed: false, decimal: true),
                             decoration: InputDecoration(
@@ -86,15 +143,7 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.all(4.0),
                         child: TextFormField(
                             controller: _controlePeso,
-                            validator: (valor) {
-                              if (valor.isEmpty) {
-                                return 'Campo peso não pode ser vazio';
-                              }
-                              if (double.parse(valor) <= 0) {
-                                return 'Por favor, entre com um peso válido';
-                              }
-                              return null;
-                            },
+                            validator: _validaPeso,
                             keyboardType: TextInputType.numberWithOptions(
                                 signed: false, decimal: true),
                             decoration: InputDecoration(
@@ -106,19 +155,16 @@ class _HomeState extends State<Home> {
                       Center(
                           child: RaisedButton(
                         onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            // If the form is valid, display a snackbar. In the real world,
-                            // you'd often call a server or save the information in a database.
-
-                            Scaffold.of(context).showSnackBar(
-                                SnackBar(content: Text('Processing Data')));
+                          if (_form.currentState.validate()) {
+                            // Se o formulário deu certo, calcula o IMC
+                            _calculaIMC();
                           }
                         },
                         textColor: Colors.white,
                         padding: const EdgeInsets.all(0.0),
                         child: Text('Calcular'),
                         color: Colors.lightBlueAccent,
-                      ))
+                      )),
                     ],
                   ),
                 )),
