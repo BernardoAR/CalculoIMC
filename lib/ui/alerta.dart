00a99bd01ea90193d1728173cf5b4dd177a4a47f
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2020-01-22 02:00:36 -0300

    Criaçao do arquivo de alerta e a classe para o chamar.
    Signed-off-by: BernardoAR <bernardo.roballo@aluno.iffar.edu.br>

---
+++ lib/ui/alerta.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Classe para tratar do alerta necessário
+ */
+class Alerta {
+  Future<void> alerta({context, titulo, texto}) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(titulo),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(texto),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
